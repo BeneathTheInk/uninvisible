8e9fdb3c1bf25beaa3d166f5e47744aa1bbf55a6
NoahWilson4 <noahrwilson4@gmail.com>
2016-02-11 17:01:27 -0700

    backbone events

--- uninvisible.js
+++ uninvisible.js
@@ -3,6 +3,7 @@
 // import * as _ from 'underscore';
 import clone from 'lodash/clone';
 import extend from 'lodash/extend';
+import EventEmitter from 'backbone-events-standalone';
 import domready from "domready";
 import * as setup from './setup.js';
 import * as openClose from './open-close.js';
@@ -17,8 +18,7 @@ import * as utils from './utils.js';
 // TODO customizable speeds, colors, cursor,..
 // TODO slideshow
 
-
-class UnInVisible extends EventEmitter {
+class UnInVisible{
 	constructor(options) {
 		this.options = extend(clone(UnInVisible.defaults), (options || {}));
 		domready(this._init.bind(this));
@@ -32,40 +32,7 @@ UnInVisible.defaults = {
 	trackSpeed: 0.085
 };
 
-_.extend(UnInVisible.prototype, setup, openClose, matrix, animations, caption, utils);
+extend(UnInVisible.prototype, setup, openClose, matrix, animations, caption, utils, EventEmitter);
 
 module.exports = new UnInVisible();
 // export default new UnInVisible(); // exports es6 module, but then in the browser you get UnInVisible = Object {default: UnInVisible, __esModule: true}
-
-
-
-
-// var _ = require('underscore');
-// var EventEmitter = require('events');
-// var util = require('util');
-// var domready = require("domready");
-
-// function UnInVisible(options) {
-// 	this.options = _.extend(_.clone(UnInVisible.defaults), (options || {}));
-// 	domready(this._init.bind(this));
-// }
-//
-// util.inherits(UnInVisible, EventEmitter);
-//
-// UnInVisible.defaults = {
-// 	document: document,
-// 	zoom: 'default',
-// 	animationSpeed: 400,
-// 	trackSpeed: 0.085
-// };
-//
-// _.extend(UnInVisible.prototype,
-// 	require('./src/setup.js'),
-// 	require('./src/open-close.js'),
-// 	require('./src/matrix.js'),
-// 	require('./src/animations.js'),
-// 	require('./src/caption.js'),
-// 	require('./src/utils.js')
-// );
-//
-// module.exports = new UnInVisible();
